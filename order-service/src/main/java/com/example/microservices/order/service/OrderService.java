a4234a92a2a87d5a43b961817d5c0b66996d45a1
Phong Vo <phongvo27ams@gmail.com>
2025-05-02 09:16:58 +0700

    Implement OpenFeign in Order Service

--- order-service/src/main/java/com/example/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/example/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.microservices.order.service;
 
+import com.example.microservices.order.client.InventoryClient;
 import com.example.microservices.order.dto.OrderRequest;
 import com.example.microservices.order.model.Order;
 import com.example.microservices.order.repository.OrderRepository;
@@ -14,13 +15,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
   private final OrderRepository orderRepository;
+  private final InventoryClient inventoryClient;
 
   public void placeOrder(OrderRequest orderRequest) {
-    Order order = new Order();
-    order.setOrderNumber(UUID.randomUUID().toString());
-    order.setPrice(orderRequest.price());
-    order.setSkuCode(orderRequest.skuCode());
-    order.setQuantity(orderRequest.quantity());
-    orderRepository.save(order);
+    var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+    if (isProductInStock) {
+      Order order = new Order();
+      order.setOrderNumber(UUID.randomUUID().toString());
+      order.setPrice(orderRequest.price());
+      order.setSkuCode(orderRequest.skuCode());
+      order.setQuantity(orderRequest.quantity());
+      orderRepository.save(order);
+    } else {
+      throw new RuntimeException("Product with SKU Code " + orderRequest.skuCode() + " is not in stock.");
+    }
   }
 }
