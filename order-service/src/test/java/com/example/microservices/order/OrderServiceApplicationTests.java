4e9af90b686cabe044f38725b77024b128c6f8ab
Phong Vo <phongvo27ams@gmail.com>
2025-05-02 10:11:42 +0700

    Use WireMock for integration testing

--- order-service/src/test/java/com/example/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,62 @@
 package com.example.microservices.order;
 
+import com.example.microservices.order.stubs.InventoryClientStub;
+
+import io.restassured.RestAssured;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.testcontainers.containers.MySQLContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+				{
+						 "skuCode": "Product_02",
+						 "price": 1000,
+						 "quantity": 1
+				}
+				""";
+
+		InventoryClientStub.stubInventoryCall("Product_02", 1);
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order placed successfully."));
+	}
 }
